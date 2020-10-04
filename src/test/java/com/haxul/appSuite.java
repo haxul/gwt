799dc988217182cc908a29362a20b5e2de41033e
haxul <s.s.starodubov@yandex.ru>
2020-10-04 11:39:35 +0400

    init

---
+++ src/test/java/com/haxul/appSuite.java
@@ -0,0 +1,14 @@
+package com.haxul;
+
+import com.haxul.client.appTest;
+import com.google.gwt.junit.tools.GWTTestSuite;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class appSuite extends GWTTestSuite {
+  public static Test suite() {
+    TestSuite suite = new TestSuite("Tests for app");
+    suite.addTestSuite(appTest.class);
+    return suite;
+  }
+}
